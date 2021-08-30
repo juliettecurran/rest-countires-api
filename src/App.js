6f95069487024a6551b2317a88c3be885b5aa7a7
Juliette Curran <juliettecurran@hotmail.com>
2021-08-30 15:53:16 +0100

    Imported Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Countries from "./Components/Countries";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Countries />
     </>
   );
